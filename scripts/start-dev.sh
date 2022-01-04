17741ccea263cbf1e78258029893b31095144394
Henri Aïdasso <henri.aidasso@energiency.com>
2022-01-04 15:51:13 +0100

    display phpmyadmin link

--- scripts/start-dev.sh
+++ scripts/start-dev.sh
@@ -5,3 +5,4 @@ docker-compose -f docker-compose.dev.yml exec app php artisan key:generate
 docker-compose -f docker-compose.dev.yml exec app php artisan migrate --seed
 
 echo "Application running at http://localhost:8000"
+echo "PhpMyAdmin http://localhost:8001"

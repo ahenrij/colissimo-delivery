1721d1c9809bee10a75c0ef1d02a211a2730aa8e
Henri Aïdasso <henri.aidasso@energiency.com>
2022-01-06 17:28:45 +0100

    menu fix

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,7 +1,7 @@
 <div class="card card-body p-0" style="height: 100%">
     <ul class="list-group menu">
         <li class="list-group-item @if(Request::route()->getName() == 'home') active @endif" onclick="window.location.href='{{ route('home') }}'">Accueil</li>
-        <li class="list-group-item @if(Request::route()->getName() == 'orders.index') active @endif" onclick="window.location.href='{{ route('orders.index') }}'">Commandes</li>
+        <li class="list-group-item @if(str_starts_with(Request::route()->getName(), 'orders')) active @endif" onclick="window.location.href='{{ route('orders.index') }}'">Commandes</li>
         <li class="list-group-item @if(Request::route()->getName() == 'about') active @endif" onclick="window.location.href='{{ route('about') }}'">A propos</li>
     </ul>
 </div>

8d6e6dfb3158850cf485fc34c72d6fbc7e311124
Henri Aïdasso <henri.aidasso@energiency.com>
2022-01-06 10:13:39 +0100

    menu active fix

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,6 +1,6 @@
 <div>
     <ul class="list-group menu">
-        <li class="list-group-item active" onclick="window.location.href='{{ route('orders.index') }}'">Commandes</li>
-        <li class="list-group-item" onclick="window.location.href='{{ route('about') }}'">A propos</li>
+        <li class="list-group-item @if(Request::route()->getName() == 'orders.index') active @endif" onclick="window.location.href='{{ route('orders.index') }}'">Commandes</li>
+        <li class="list-group-item @if(Request::route()->getName() == 'about') active @endif" onclick="window.location.href='{{ route('about') }}'">A propos</li>
     </ul>
 </div>

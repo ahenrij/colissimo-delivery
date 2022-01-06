3ded0df0208521207f3e39cea5edf69260120d51
Henri Aïdasso <henri.aidasso@energiency.com>
2022-01-06 10:51:14 +0100

    about updated

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,10 +2,12 @@
 
 @section('content')
 
-<div class="container mt-5">
-    <h3 class="text-center">A propos</h3>
+<div class="container">
+    <div class="card p-5">
+        <h3 class="text-center">A propos</h3>
     <p style="font-size: 1.2em" class="text-center mb-0">Post Colissima, application de suivi de colis.</p>
-    <p class="text-center mt-1">Version 1.0</p>
+    <small class="text-center mt-2 mb-0">VERSION 1.0</small>
+    </div>
 </div>
 
 @endsection

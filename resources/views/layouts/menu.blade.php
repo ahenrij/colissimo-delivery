e4ec9e00617543551c5d57c05eea77bf89414168
Henri Aïdasso <henri.aidasso@energiency.com>
2022-01-06 11:05:04 +0100

    menu add home

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,5 +1,6 @@
 <div>
     <ul class="list-group menu">
+        <li class="list-group-item @if(Request::route()->getName() == 'home') active @endif" onclick="window.location.href='{{ route('home') }}'">Accueil</li>
         <li class="list-group-item @if(Request::route()->getName() == 'orders.index') active @endif" onclick="window.location.href='{{ route('orders.index') }}'">Commandes</li>
         <li class="list-group-item @if(Request::route()->getName() == 'about') active @endif" onclick="window.location.href='{{ route('about') }}'">A propos</li>
     </ul>

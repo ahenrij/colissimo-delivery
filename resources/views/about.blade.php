60356d4b87efa8e31ae3a7e2a0d0f5d8f5ff21d1
Henri Aïdasso <henri.aidasso@energiency.com>
2022-01-06 10:52:08 +0100

    about updated

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,7 +5,7 @@
 <div class="container">
     <div class="card p-5">
         <h3 class="text-center">A propos</h3>
-    <p style="font-size: 1.2em" class="text-center mb-0">Post Colissima, application de suivi de colis.</p>
+    <p style="font-size: 1.2em" class="text-center mb-0">{{ config('app.name', 'Laravel') }}, application de suivi de colis.</p>
     <small class="text-center mt-2 mb-0">VERSION 1.0</small>
     </div>
 </div>
